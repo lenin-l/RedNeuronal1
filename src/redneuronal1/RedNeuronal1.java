43a0992ae2b965f9f8e6afef69ed7c5f4b9b6005
lenin-l <65566941+lenin-l@users.noreply.github.com>
2020-05-26 07:19:39 -0500

    RedNeuronal1

---
+++ src/redneuronal1/RedNeuronal1.java
@@ -0,0 +1,50 @@
+/*
+ * Implemente un algoritmo que le permita: 1). desplegar el nombre del proyecto de su paper
+2. Que es una red neuronal.
+3). Cuales son los elementos que forman una red neuronal.
+4). Cómo utilizaron la red neuronal en el proyecto que usted ha seleccionado.
+5). Cómo utilizaría usted la red neuronal como mejoramiento al proyecto propuesto
+6). En que consistió el entrenamiento utilizando una red neuronal, mencione el algoritmo 
+utilizado.
+ */
+
+package redneuronal1;
+
+/**
+ *
+ * @author:Lenin Alexander Carrillo Imbaquingo 
+ */
+import java.util.Scanner;
+public class RedNeuronal1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Scanner objeto=new Scanner (System.in);
+        System.out.println("\t Desplegar El Nombre Del Proyecto De Su Paper");
+        String nombreProyecto=objeto.nextLine();
+        
+            System.out.println("\t Que es una Red Neuronal");
+        String impre=objeto.nextLine();
+        
+        System.out.println("\t Cuales son los elementos que forman una red neuronal");
+        String elementosqueForman=objeto.nextLine();
+        
+        System.out.println("\t Cómo utilizaron la red neuronal en el proyecto que usted ha seleccionado");
+        String comoUtilizarono=objeto.nextLine();
+        
+        System.out.println("\t Cómo utilizaría usted la red neuronal como mejoramiento al proyecto propuesto");
+        String mejoramientoProyecto=objeto.nextLine();
+        
+        System.out.println("\t En que consistió el entrenamiento utilizando una red neuronal, mencione el algoritmo utilizado");
+        String entrenamientoUtilizado=objeto.nextLine();
+        
+        
+        
+        
+      
+    }
+    
+}
